b9301cede7066b6dd056e7bea7e1553821839120
Natalya Matiukhina <nmatiukhina@rbc.ru>
2021-03-31 20:49:31 +0300

    lesson 2

--- src/components/app.js
+++ src/components/app.js
@@ -2,23 +2,40 @@ import React, { useEffect, useState, useCallback } from "react";
 
 import "./app.scss";
 
+import { AUTHORS } from "../utils/constants";
+import Form from "./form";
+
 const App = ({ title }) => {
 
-    const [messages, addMessages] = useState(['Привет', 'Hello', 'Hola', 'Hallo']);
+    const [messages, addMessage] = useState([]);
+
+    const setMessage = useCallback((newMessage) => {
+        addMessage((prevMessage) => [...prevMessage, newMessage])
+    }, []);
+
+    useEffect(() => {
+        let timeout;
+
+        if(messages[messages.length -1]?.author !== AUTHORS.BOT) {
+            timeout = setTimeout(() => {
+                setMessage({text: 'Привет!', author: AUTHORS.BOT})
+            }, 500);
+        }
+
+        return () => clearTimeout(timeout);
+    }, [messages]);
 
     return (
-        <div className='full-screen'>
+        <div className="full-screen">
             <div>
                 <h1>{title}</h1>
                 <br />
-                <div className='messages'>
-                    {messages.map(text => <span className='messages__item'>{text}</span>)}
+                <div className="messages">
+                    {messages.map(({ text, author }, i) => (
+                        <span className="messages__item" key={i}>{author}: {text}</span>
+                    ))}
                 </div>
-                <span 
-                className='button-line'
-                onClick={() => addMessages([...messages, 'Hi'])}>
-                    Новое сообщение
-                </span>
+                <Form onSetMessage={setMessage} />
             </div>
        </div>
    );
